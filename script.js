48e1923c564a6a7413bd0874a18c2c21605a09db
Sistemas <adrianrincon2306@gmail.com>
2026-03-25 16:30:21 -0400

    svgs

---
+++ script.js
@@ -0,0 +1,180 @@
+// ── SVG kid snippets (CON VIEWBOX RECORTADOS) ──
+const svgs = {
+  // Niña Rosa (I)
+  kidI: `<svg class="kid-i-svg" viewBox="90 40 60 175" xmlns="http://www.w3.org/2000/svg">
+    <path id="pink_girl" d="M108.64,105.67l-.69,2.11-.09,1.17-3.23-1.43-2.22,1.18.66,4,.38.92,1.58.82,1.4.24,1.88.19,2,.81,1.66-.18,2.18-.77.34-.2.34-.26,1.21-.92.77-1,.13-.41.44-.62.41-.95.19-.74.17-.76.06-.46.06-.41.15-1.24.08-1.43.05-1.29v-.64l-.53-2.37-1.61-3.37-1.6-2.06-.85-3.74.48-2.52.87-.93,2,1.37,1.2,3.4,1.24,4.47,1.32,4.75L122.6,108l1.9,6.84s.11.43,1.23,4.41,2,7.34,2,7.34a6,6,0,0,1-.8,4.18c-1.3,2.45-5,10.19-5,10.19l-2.54,5.45-2.57,5a9.91,9.91,0,0,0-.27,5.59,2.94,2.94,0,0,1-1.06,3.64s-1.12,1.21-.9,2a2.85,2.85,0,0,0,1.09,1.92,15.7,15.7,0,0,0,2.94,1.39l2.06.34,2.09.45s.45-.28.8.87.88,4.08.88,4.08l-.34,5.5-.71,3.5-.63,2.58-.68,3.5-.39,3s-.31,5.56-.2,7.28.68,6.43.68,6.43.1,4.29,0,6.18-.08,3.39-.08,3.39,0,.51.68.6,3.67,0,3.67,0l8.6-.43s12.9.3,13.81-2.26-2.36-5-2.36-5l-2.74-2.06s-9,.58-9.51-2.14a14.15,14.15,0,0,1-.35-3.75s-.14-4.19-.28-5.74-.18-4.27-.18-4.27,1.28-6.45,1.62-7,2.08-5.69,2.08-5.69l2.46-5.54a8.84,8.84,0,0,1,2.69-.53,34.13,34.13,0,0,0,4,.08l3.47-.11s2.09,0,2.09-.34a16.13,16.13,0,0,0-.74-2.21,6.49,6.49,0,0,1-.59-2.72,10.19,10.19,0,0,1,.82-3.35,19,19,0,0,1,2.08-2.92c.55-.38-.6-2.67-.6-2.67l-1.2-2.27s-2.12-4-2.2-5.16a15.13,15.13,0,0,1-.11-3.07,17.15,17.15,0,0,1,.36-3.2c.46-1.53.64-2.36.64-2.36s.86-5.93.86-7,.21-7.09,0-8.2-.69-4.34-.69-4.34l-.74-3.23-1.24-3.73-1.25-3.81-.9-3.68-.65-4.54s-.23-5.87,0-6.5.75-5.08,1-6.08,2-5.9,2-7.25.72-4.21.68-5.65.28-6.22.28-6.22.5-7,.6-7.67,1-5.62,1-5.62a2.91,2.91,0,0,0,.21-1.21c-.08-.49-.91-.74-.91-.74l-1.73-2-3-3.19s-2.83-2.73-3.3-1.82,1.6,3.31,1.6,3.31l.57,2.07-.11,2.29.46,2.18-.23,5.85L143,70.68l-2.72,7.83-1.37-6.27s-3.87-2.69-7.61-3c-5.84-.58-7.32,5.72-6.42,1.14.47-2.38.66-15.93.91-16.87a31,31,0,0,1,2-4.42c.27-.39-1.29-1.88-1.29-1.88l-3.24-1-1.46-1.18-1.06-1.21-1.69-1s-1.51-.83-1.52.06c.34.16,1.45,4.47,2.81,5.44.8.58-.12,7.11-.12,7.11l-.9,6.54L118,71.3l-1.51,6.9-1.09,4.59-1.11,1.79-2.9,1.93-2.34,3.08-1.9,3.29,0,2.23" fill="currentColor"/></svg>`,
+
+  // O con niña leyendo
+  swirlO: `
+    <svg class="kid-svg kid-svg-swirl" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M20 20 C20 14 26 10 30 14 C34 18 32 26 26 28 C20 30 12 26 10 20 C8 14 12 6 20 4 C28 2 36 8 38 18" stroke="#F5A623" stroke-width="4" stroke-linecap="round" fill="none"/>
+    </svg>
+`,
+
+  // Niño Y (Rojo)
+  kidY: `<svg class="kid-y-svg" viewBox="65 25 125 195" xmlns="http://www.w3.org/2000/svg">
+    <g id="red_y">
+      <path id="torso" d="M135.24,74.65c5.27-5.69,5.45-11.6,6.26-17.58s-1.37-9.64-5.53-12.74a11.58,11.58,0,0,0-14.36.37c-3.85,3.12-6.31,7.08-4.19,12.5a6.3,6.3,0,0,1,.26,3.17c-.74,4.37.11,8.13,3.41,11.25a2.41,2.41,0,0,1,.83,2.77,1.8,1.8,0,0,1-1.88.17L111.13,90a28.94,28.94,0,0,1,4.14,14.81A331,331,0,0,1,114.09,138c-1.3,14.81,2.85,29.36,2.67,44.15a10.26,10.26,0,0,0,1,4.42c1.58,3.28,1.53,6.69,1.41,10.18-.11,2.83-.78,5.91.08,8.43,1.92,5.65.47,10.13-3,14.49-2.06,2.61-2,5.27.82,7.35,2.34,1.75,6.44,1.32,8-1.21,1.86-3,4.46-5.23,6.86-8.07-.39-6.57.37-13.26.64-19.94.27-6.45-1.52-13,1.15-19.29a78.15,78.15,0,0,0,3.15,16,14,14,0,0,1-.09,9.49,19.12,19.12,0,0,0,.47,14,28.26,28.26,0,0,1,2.46,10.67c0,4.61,3,6.54,7.31,5.15a5.91,5.91,0,0,0,3.18-8.43,19.9,19.9,0,0,1-2-12.48c.77-5.34.5-10.79,2.6-16a7.77,7.77,0,0,0-.08-4.5,56.25,56.25,0,0,1-1.11-20.58c.86-7.52.06-15.22.75-22.77a42.37,42.37,0,0,0-.15-11.58c-.68-3.8-2.42-7.56-1.56-11.52,1.33-6.09.51-12.23-.41-18.11a44.9,44.9,0,0,1,1.55-21.19l-8.46-14.65A13.19,13.19,0,0,1,135.24,74.65Z" fill="currentColor"/>
+      <path id="brazo-izq" d="M119.46,74.29c-3.74-1.58-6.08-4.65-8.44-7.79C101.1,53.37,92.1,39.4,78.68,29.42c-1.28-1-2.1-2.68-4.41-2.55,1.2,4.08,5.93,6.69,3.89,10.86,1.48,2.77,4.53,2.71,5.35,5.42,1.51,5,5.36,8.62,7.94,12.82,3.38,5.51,7,11.08,9.88,16.77a131.08,131.08,0,0,0,8.44,15.06c.49.71.94,1.43,1.36,2.15L120,74.52Z" fill="currentColor"/>
+      <path id="brazo-der" d="M177.33,33.83c1.55-2.29,3.41-4.43,4.72-8.62-8.09,5.79-13.62,12.12-19,18.43C156,52,147.85,59.44,143.19,69.69a7,7,0,0,1-1.77,2.39l8.46,14.65a51.58,51.58,0,0,1,3.79-8.8c2.72-5,5.24-10.1,9-14.39.75-.87,1.45-1.57,1.37-2.7-.23-3.13,1.62-5.45,3.31-7.56a32.46,32.46,0,0,0,6.3-12c1.88-2.22,5.19-1.54,5.71-4.35C179.57,35.15,177.16,35.43,177.33,33.83Z" fill="currentColor"/>
+    </g>
+  </svg>`,
+
+  // Niña Verde Caminante
+  walker: `<svg viewBox="80 30 100 180" xmlns="http://www.w3.org/2000/svg">
+    <g id="kid_green">
+      <path id="body" d="M153.57,176.18c14.21-3.32,14.13-2.64,15.35-2.32,6.05,1.57-2.87-22.11-2.62-26.28.43-7.21,1.39-7.37-1.22-14.13-1.05-2.71,1.15-2.7.6-5.57-1.23-6.38,3.17-5.91,2.54-12.33-1.11-11.29-.32-11.34-2.63-22.48-1.07-5.11-3.34-6.84.6-10.34,1.62-1.44-5.5-8.94-7.32-11.39-.85-1.25,1-4.43,4.57-3.13,2.45,2,6,18.73,7.42,21.18,1,2.67,11.82,11.15,16.09-2.49-13.51-5.25-6.15-26.68-19.61-30-2.14-.88-1.53,0-3.34-1.13-.7-.94-.39.42-.94-1.58a15.72,15.72,0,0,0-1.2-4.39C156.52,35.39,148.57,36.05,148,35.55c-4.52-.48-5.76-.53-6.93-.6a15.87,15.87,0,0,0-5.17.31,57.11,57.11,0,0,0-8.63,2c-3.18,1.42-2.36,1-5.27,3.63l-1.75,2.48c0,.13-.41.45-1.24,1.78-1.4,3.48-.79,2.93-1,3.41.13.67-.28,1-.15,2.18a2.29,2.29,0,0,1-.13,1,15.72,15.72,0,0,1-.63,1.69c-1.15,4.62-.79,6.37,1.57,11.34.64,1.36,2.21-1.35,2.8-.85.16.15-.55.57,1.44,2.32,2.63,2.3,2.43,2.61,2.08,6.07-.24,2.43,2.51,2.52,3.08,3.1s5.95,6,9,5.46c2.15-.4,3.37-1.29,3.1.82-.25,2-3-1.22-6,6-1,3.23-2,4.61-2.66,7-1,6.13-.53,3.79-1,6.13-.26,1.36-.71,2.74-.88,4.2-.38,2.8.17,3.82,0,5a23.66,23.66,0,0,0,.07,5.22c-.44,2.88-.44,2.88-.75,6-1.92,4.67-1.08,1.76-.92,5.81,0,3.35-.09,3.12.49,4.32,1.86,3.79-.57,5.62-2.48,9.43l-.61,2.07c-1.45,6.73,2-4.48-5,15.52.32,3.43-3.85,2.6-4,3.52s.89,7.18,8.84,8.71c2,.57,7.89,2.47,9.77,4.82" fill="#429e3c" fill-rule="evenodd"/>
+      <path id="leg_right" d="M155.88,171.29c.78.58-6.76-7-11.76-6-2.75.91-4.34,5.91-4.38,6.62a9.55,9.55,0,0,0-.2,1.66c.84,8.18,2.49,7.64,10.82,18,.72.9,7.36,9.19,7.8,12.08.77,5.16-9.28,9.24-9,13.33.12,1.62,1,1.83,3.17,3.11,3,1.75,8.77-2.51,11.16-4.24,4.12-3,18.23-8.27,4.18-15.94-4.44-2.42-1-12.52-7.47-19.77-1.08-1.21-1.1-4.44-2.94-6.87" fill="#429e3c" fill-rule="evenodd"/>
+      <path id="leg_left" d="M148.85,154c2.7-1.87,4.76-4.74,4.76-6.42s-.92-5-3.06-5.81a26.56,26.56,0,0,0-8.26-1.38,24.51,24.51,0,0,0-8.4,1.69l-8.68,2.58-2.48,1.5c-2.71,2.36-4.77,3.14-5.74,4-1.18,1.09.67-.26-5.89,4.4-3.75,2.66-4.93,1.71-6.38,6-1.69,5-7.46,12.37-18.44,27.14-8.35,11.23-16.43,1-21.79,2.56-1.16.33-1.21,0-2,1.13,0,.21-.52.79-.54,1-.21,2.47-.24,1.93.26,4,2.1,1.41,3.66,3.7,7.13,5.52,9.15,4.78,18.75,15.86,21.33,1.18,1.79-10.15,5.68-7.76,14.13-16.22,8.89-8.91,3.26-15.68,13.5-18,5.36-2.07,5.43-1.88,6-1.56,12.34-4.27,19.88-10.48,21.85-11.49Z" fill="#429e3c" fill-rule="evenodd"/>
+      <path id="arm" d="M136.41,112.52l-16.55,5-6.78,3.1-3.16,1.21a6.08,6.08,0,0,1-3.23.17c-1.2-.47-5.66,0-5.66,0l-3,.64a11.74,11.74,0,0,0-2.88,1.64A2.27,2.27,0,0,0,94.6,126a5.24,5.24,0,0,0,.39,1.19c.21.37,1.25,1.91,1.25,1.91l1.16,1.75L99.16,133l1.38,1.13a3.54,3.54,0,0,0,1.67.55,27,27,0,0,0,3.35-.82l2.83-1.18s1.49-.38,1.91-.43a17.58,17.58,0,0,1,3.49-.1c.07.21,2.42,0,3.15-.08s6.32-1.05,6.32-1.05l11.62-3,14-4.55a14.39,14.39,0,0,0,6-11c.46-7.57,1.83-23.07,1.83-23.07a5,5,0,0,0-1.11-3,4.27,4.27,0,0,0-2.1-1.14s-.8-.61-4.13.83A8.27,8.27,0,0,0,145,90.89l-2.18,8.48-1,7a10.09,10.09,0,0,1-2.44,4.82,5.07,5.07,0,0,1-3,1.3" fill="#429e3c" fill-rule="evenodd"/>
+    </g>
+  </svg>`,
+
+  // Letras básicas que ya tenías
+  kidK: `<svg class="kid-svg" viewBox="0 0 40 58" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="6" r="5.5"/><path d="M17 12 L15 30 L19 30 L20 23 L21 30 L25 30 L23 12 Z"/><path d="M16 14 L4 6 L5 4 L17 12 Z"/><path d="M24 14 L36 6 L37 8 L25 16 Z"/><path d="M16 30 L8 46 L12 47 L19 33 Z"/><path d="M24 30 L32 46 L28 47 L21 33 Z"/></svg>`,
+  kidD: `<svg class="kid-svg" viewBox="0 0 42 58" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><circle cx="24" cy="7" r="5.5"/><path d="M20 13 L17 30 L21 30 L24 22 L27 30 L31 30 L28 13 Z"/><path d="M18 15 L8 10 L9 8 L20 13 Z"/><path d="M29 14 L38 20 L37 22 L28 16 Z"/><path d="M18 30 L13 46 L17 47 L22 33 Z"/><path d="M29 30 L36 44 L32 45 L24 33 Z"/></svg>`,
+  kidS: `<svg class="kid-svg" viewBox="0 0 40 58" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><circle cx="22" cy="6" r="5.5"/><path d="M18 12 L15 29 L19 29 L22 21 L25 29 L29 29 L26 12 Z"/><path d="M16 14 L5 18 L4 16 L15 12 Z"/><path d="M27 13 L37 8 L38 10 L28 15 Z"/><path d="M16 29 L10 45 L14 46 L20 32 Z"/><path d="M27 29 L35 43 L31 44 L23 32 Z"/></svg>`,
+  kidB: `<svg class="kid-svg" viewBox="0 0 44 58" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><circle cx="22" cy="6" r="5.5"/><path d="M18 12 L16 30 L20 30 L22 22 L24 30 L28 30 L26 12 Z"/><path d="M17 13 L4 9 L5 7 L18 11 Z"/><path d="M27 13 L40 9 L39 7 L26 11 Z"/><path d="M17 30 L11 46 L15 47 L20 33 Z"/><path d="M27 30 L33 46 L29 47 L24 33 Z"/></svg>`,
+  kidR: `<svg class="kid-svg" viewBox="0 0 40 58" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="6" r="5"/><path d="M16 12 L14 30 L18 30 L20 22 L22 30 L26 30 L24 12 Z"/><path d="M15 14 L5 6 L7 4 L16 12 Z"/><path d="M25 13 L35 18 L34 20 L24 15 Z"/><path d="M14 30 L9 46 L13 47 L18 33 Z"/><path d="M26 30 L28 46 L24 47 L21 33 Z"/></svg>`,
+  curledO: `<svg class="kid-svg kid-svg-curled" viewBox="110 100 80 80" xmlns="http://www.w3.org/2000/svg">
+      <g id="rding_kid">
+        <path id="reading" d="M144.51,154.07s-2,4.1-2.17,4.7-2.9.58-2.9.58l-4.23-.67s-2.87-.32-3.93,1a11.26,11.26,0,0,0-2.22,5.24,22.47,22.47,0,0,1-1.77,6.27c-.9,1.84-1.46,9.57-1.46,9.57a20.54,20.54,0,0,1-1.3,3.08c-.5.76-4.9,2.69-4.9,2.69l-2.94,1.74s-1.52,2-1.33,2.71,0,2.51.84,3.09,4.34-.45,6-.9c2.61-.7,7.34.35,7.34.35l3.45.07a13.43,13.43,0,0,0,2.81-1.45,5.35,5.35,0,0,0,1.9-3.25c.41-2-1.06-5.68-.82-5.86a37.52,37.52,0,0,0,.82-4.89s2.28-3.85,3.51-1.57,11.38,7.51,11.38,7.51c.36.36,1.53.44,3.32,1.72a8.89,8.89,0,0,1,2.52,3.09s1.9.92,3.1,1.38a29.93,29.93,0,0,0,3.67,1,12,12,0,0,0,5.46-2.06c2.2-1.75,7.51-11.19,7.51-11.19l2.56-22.28.45-13.63-2.31-7.56s-6.78-2-6.55-4.58c.88-9.78,5.77,2.9,9.42,3.53,1.65.28,3.38-.78,5.67-4.91l-5.2-4.89S177.62,117,176,116a4.8,4.8,0,0,0-4.7-.62s-.5,1.84-2.6-5.19c-.57-1.91-5.24-3.93-5.24-3.93a8.57,8.57,0,0,0-5.07-1.8,29,29,0,0,0-5.86,1.14s-4.51,2.2-5.9,3.67a18.55,18.55,0,0,0-3.8,5.37c-.73,1.89-1.74,4.51-1.61,6.16a7.68,7.68,0,0,0,.93,3,9.75,9.75,0,0,1,1.42,1.45,27.76,27.76,0,0,1,2.05,3.41c.35.76,2.23,5.07,2.51,5.6a25.38,25.38,0,0,0,2.44,3.33c.47.42,7.13,0,6.52,1.86s-6.33,5.07-6.33,5.07c-.49.77-2.44,3.33-2.73,3.09" fill="#e91e8c"/>
+        <path id="book" d="M148,147.55a44.34,44.34,0,0,0-5.75-3.89c-3-1.68-8.07-4.43-8.07-4.43l-3.49-2.09a10.31,10.31,0,0,0-1.43,4c-.26,5.9,5.62,6.91,7.4,8.24,2.06,1.53,7.83,4.73,7.83,4.73" fill="#e91e8c"/>
+      </g>
+    </svg>`,
+  starA: `<svg class="kid-svg kid-svg-star" viewBox="0 0 40 40" fill="#F5A623" xmlns="http://www.w3.org/2000/svg"><polygon points="20,3 24,14 36,14 27,21 30,33 20,26 10,33 13,21 4,14 16,14"/></svg>`,
+  kidC: `<svg class="kid-svg" viewBox="0 0 40 58" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="5" r="5"/><path d="M16 11 L13 28 L17 28 L20 20 L23 28 L27 28 L24 11 Z"/><path d="M14 13 L4 6 L5 4 L15 11 Z"/><path d="M26 13 L36 6 L35 4 L25 11 Z"/><path d="M14 28 L8 44 L12 45 L18 31 Z"/><path d="M26 28 L32 44 L28 45 L22 31 Z"/></svg>`,
+  kidA2: `<svg class="kid-svg" viewBox="0 0 38 58" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><circle cx="19" cy="6" r="5"/><path d="M15 12 L13 29 L17 29 L19 21 L21 29 L25 29 L23 12 Z"/><path d="M13 13 L4 8 L5 6 L14 11 Z"/><path d="M25 13 L33 20 L32 22 L24 15 Z"/><path d="M13 29 L7 45 L11 46 L17 32 Z"/><path d="M25 29 L27 45 L23 46 L19 32 Z"/></svg>`,
+  kidD2: `<svg class="kid-svg" viewBox="0 0 40 58" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="6" r="5"/><path d="M16 12 L14 29 L18 29 L20 22 L22 29 L26 29 L24 12 Z"/><path d="M14 14 L4 22 L3 20 L13 12 Z"/><path d="M26 14 L36 10 L37 12 L27 16 Z"/><path d="M14 29 L7 44 L11 45 L17 32 Z"/><path d="M26 29 L29 44 L25 45 L21 32 Z"/></svg>`,
+  kidE: `<svg class="kid-svg" viewBox="0 0 40 58" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="5" r="5.5"/><path d="M16 11 L14 28 L18 28 L20 20 L22 28 L26 28 L24 11 Z"/><path d="M15 12 L3 5 L4 3 L16 10 Z"/><path d="M25 12 L37 5 L38 7 L26 14 Z"/><path d="M15 28 L9 44 L13 45 L18 31 Z"/><path d="M25 28 L30 44 L26 45 L22 31 Z"/></svg>`,
+  kidM: `<svg class="kid-svg" viewBox="0 0 38 58" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><circle cx="19" cy="6" r="5"/><path d="M15 12 L13 29 L17 29 L19 22 L21 29 L25 29 L23 12 Z"/><path d="M13 14 L4 8 L5 6 L14 12 Z"/><path d="M25 14 L33 20 L32 22 L24 16 Z"/><path d="M13 29 L8 44 L12 45 L17 32 Z"/><path d="M25 29 L27 45 L23 46 L21 32 Z"/></svg>`,
+};
+
+// ── Configurador de Letras (Añadido identificador ID para colisiones) ──
+const kidsboroLetters = [
+  { ch: 'K', color: '#F5A623', id: 'l-k' },
+  { ch: 'I', color: '#E91E8C', kid: 'kidI', replace: true, id: 'l-i' }, // Niña Rosa
+  { ch: 'D', color: '#4ba449', id: 'l-d' },
+  { ch: 'S', color: '#00b6ab', id: 'l-s' },
+  { ch: 'B', color: '#ed1f24', id: 'l-b' },
+  { ch: 'O', color: '#4ba449', kid: 'swirlO', id: 'l-o1' }, // Niña Leyendo
+  { ch: 'R', color: '#00b6ab', id: 'l-r' },
+  { ch: 'O', color: '#E91E8C', kid: 'curledO', id: 'l-o2' },
+];
+
+const academyLetters = [
+  { ch: 'A', color: '#4ba449', kid: 'starA', id: 'l-a1' },
+  { ch: 'C', color: '#E91E8C', id: 'l-c' },
+  { ch: 'A', color: '#F5A623', id: 'l-a2' },
+  { ch: 'D', color: '#00b6ab', id: 'l-d2' },
+  { ch: 'E', color: '#4ba449', id: 'l-e' },
+  { ch: 'M', color: '#F5A623', id: 'l-m' },
+];
+
+// ── Constructor de Letras (Ahora son LINKS <a>) ──
+function buildLetter({ ch, color, kid, replace, id }, delay) {
+  const link = document.createElement('a'); // Ahora son enlaces
+  link.href = "#"; // Aquí pones tu destino final
+  link.className = `letter letter-out`;
+  link.id = id;
+  link.style.color = color;
+
+  if (replace) {
+    link.classList.add('letter-svg-only');
+    link.innerHTML = svgs[kid];
+  } else {
+    link.textContent = ch;
+    if (kid) {
+      const wrap = document.createElement('span');
+      wrap.className = 'kid-wrap';
+      wrap.innerHTML = svgs[kid];
+      link.appendChild(wrap);
+    }
+  }
+
+  setTimeout(() => link.classList.replace('letter-out', 'letter-in'), delay);
+  return link;
+}
+
+const rowK = document.getElementById('row-kidsboro');
+kidsboroLetters.forEach((def, i) => rowK.appendChild(buildLetter(def, i * 80)));
+
+const rowA = document.getElementById('row-academy');
+academyLetters.forEach((def, i) => rowA.appendChild(buildLetter(def, 640 + i * 80)));
+
+// Niño Y (Rojo)
+const yDelay = 640 + 6 * 80;
+const ySpan = document.createElement('a');
+ySpan.href = "#";
+ySpan.className = 'letter letter-y letter-out';
+ySpan.id = 'l-y';
+ySpan.style.color = '#ed1f24';
+ySpan.innerHTML = svgs.kidY;
+setTimeout(() => ySpan.classList.replace('letter-out', 'letter-in'), yDelay);
+rowA.appendChild(ySpan);
+
+// Subtítulo
+const sub = document.getElementById('subtitle');
+const text = 'WHERE KIDS ACQUIRE KNOWLEDGE AND FITNESS';
+text.split('').forEach((ch, i) => {
+  const s = document.createElement('span');
+  s.className = 'sub-char';
+  s.style.animationDelay = `${1400 + i * 28}ms`;
+  s.textContent = ch === ' ' ? '\u00A0' : ch;
+  sub.appendChild(s);
+});
+
+// ── Inyectar a la Niña Verde Caminante ──
+const logoContainer = document.getElementById('logo');
+const walkerDiv = document.createElement('div');
+walkerDiv.className = 'green-walker';
+walkerDiv.innerHTML = svgs.walker;
+logoContainer.appendChild(walkerDiv);
+
+
+// ── LÓGICA DEL DIRECTOR DE ESCENA (Colisiones) ──
+window.addEventListener('load', () => {
+  const letters = document.querySelectorAll('.letter');
+  const walker = document.querySelector('.green-walker');
+  const pinkGirl = document.getElementById('l-i');
+  const redSpiral = document.getElementById('espiral-roja');
+
+  function checkCollisions() {
+    const walkerRect = walker.getBoundingClientRect();
+    const walkerCenterX = walkerRect.left + (walkerRect.width / 2);
+
+    letters.forEach(letter => {
+      // Ignoramos la 'Y' porque la niña verde no llega hasta allá
+      if (letter.id === 'l-y') return;
+
+      const rect = letter.getBoundingClientRect();
+
+      // Si la niña verde camina justo por debajo de la letra
+      if (walkerCenterX > rect.left && walkerCenterX < rect.right) {
+
+        // Caso Especial: Letra I (Niña Rosa)
+        if (letter.id === 'l-i') {
+          walker.classList.add('pushing-up');
+          pinkGirl.classList.add('pushed-high');
+          redSpiral.classList.add('blinking-fast');
+        } else {
+          // El resto de letras hacen el efecto Zoom
+          letter.classList.add('letter-zoom-active');
+        }
+      } else {
+        // Cuando la niña ya pasó, se apagan los efectos
+        letter.classList.remove('letter-zoom-active');
+        if (letter.id === 'l-i') {
+          walker.classList.remove('pushing-up');
+          pinkGirl.classList.remove('pushed-high');
+          redSpiral.classList.remove('blinking-fast');
+        }
+      }
+    });
+
+    requestAnimationFrame(checkCollisions);
+  }
+
+  // Empezar el ciclo infinito de rastreo
+  requestAnimationFrame(checkCollisions);
+});
